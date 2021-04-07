1c4faaf6e991a6772ec38f57caa6b070a9599c5c
Anders Jansson <andersjansson1994@gmail.com>
2021-04-07 13:04:16 +0200

    initial commit

---
+++ Test/PersonTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+class PersonTest {
+    private String name;
+    private int age;
+    private Person person;
+
+    @BeforeEach
+    void setup(){
+        name = "John";
+        age = 34;
+        person = new Person(name, age);
+    }
+    @Test
+    void constructor(){
+        age = -1;
+        int expectedAge = 0;
+        person = new Person(name, age);
+        Assertions.assertEquals(expectedAge, person.getAge());
+    }
+    @Test
+    void getName() {
+        String returnedName = person.getName();
+        Assertions.assertEquals(name, returnedName);
+    }
+
+    @Test
+    void setName() {
+        String newName = "Conny";
+        person.setName(newName);
+        Assertions.assertEquals(newName, person.getName());
+    }
+
+    @Test
+    void getAge() {
+        int returnedAge = person.getAge();
+        Assertions.assertEquals(age, returnedAge);
+    }
+
+    @Test
+    void setAge() {
+        int newAge = 63;
+        person.setAge(newAge);
+        Assertions.assertEquals(newAge, person.getAge());
+    }
+    @Test
+    void setAgeNegative(){
+        int newAge = -1;
+        int expectedAge = 0;
+        person.setAge(newAge);
+        Assertions.assertEquals(expectedAge, person.getAge());
+    }
+
+    @Test
+    void reverseName() {
+        StringBuilder stringBuilder = new StringBuilder();
+        String reversed = stringBuilder.append(name).reverse().toString();
+        Assertions.assertEquals(reversed, person.reverseName(name));
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,47 @@
+// Model class of a person
+public class Person {
+
+//    This is the attributes of a person
+    private String name;
+    private int age;
+
+//    Constructor
+    public Person(String name, int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+        this.name = name;
+    }
+
+//    returns the persons name
+    public String getName() {
+        return name;
+    }
+
+//    Sets a new name for the person
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    returns the persons age
+    public int getAge() {
+        return age;
+    }
+
+//    Sets a new age for the person
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+    }
+
+//    Returns the persons name in reverse order
+    public String reverseName(String name) {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append(name).reverse().toString();
+    }
+}
